864e93598bbdca0c0fc90ef3d8a9e238b94fb479
i1SLOWFire <vano2001878@gmail.com>
2026-09-20 15:43:39 +0900

    добавил проверку статуса задачи в check_task

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import todo_manager
 
-manager = todo_manager.TaskManager()
+manager = todo_manager.TaskManager("tasks.json")
 
 if __name__ == "__main__":
     manager.main()
--- todo_manager.py
+++ todo_manager.py
@@ -1,6 +1,7 @@
 from libs.console_menu.menu import ConsoleMenu
 from libs.json_storage.storage import JSONStorage
 
+
 class Task:
     def __init__(self, task_id, title, done=False):
         self.id = task_id
@@ -10,18 +11,20 @@ class Task:
     def to_dict(self):
         # Превращает в словарь для JSON
         return {"id": self.id, "title": self.title, "done": self.done}
+
     @staticmethod
     def from_dict(data):
         # Создаёт задачу из словаря
         return Task(data["id"], data["title"], data["done"])
 
+
 class TaskManager:
-    def __init__(self):
+    def __init__(self, storage_name, password=None):
         self.next_id = 0
         self.menu_name = "menu.json"
-        self.password = "1"
+        self.password = password
         self.menu = ConsoleMenu(self.menu_name, self, self.password)
-        self.storage_name = "tasks.json"
+        self.storage_name = storage_name
         self.storage = JSONStorage(self.storage_name, Task)
 
     def main(self):
@@ -36,27 +39,42 @@ class TaskManager:
                 print(f"[ ] Задача №{item.id}: {item.title}")
             else:
                 print(f"[X] Задача №{item.id}: {item.title}")
-        input("\nНажмите Enter для того, чтобы продолжить")
+        input("\nНажмите Enter для того, чтобы продолжить.")
 
     def add_task(self):
-        self.storage.load()
-        title = (input("Введите новую задачу: "))
-        items = self.storage.items
-        self.next_id = len(items) + 1
-        self.storage.add_item(Task(self.next_id, title))
+        while True:
+            self.storage.load()
+            title = (input("Введите новую задачу: "))
+            if title == "":
+                print("Вы ничего не написали.")
+                continue
+            items = self.storage.items
+            self.next_id = len(items) + 1
+            self.storage.add_item(Task(self.next_id, title))
+            return
 
     def check_task(self):
         while True:
             try:
                 self.next_id = int(input("Введите номер выполненной задачи: "))
             except ValueError:
-                print("\nОшибка: введите число.")
+                print("Ошибка: введите число.")
                 continue
             self.storage.load()
+            if not self.storage.items:
+                print("Список задач пуст.")
+                return
+            elif self.next_id < self.storage.items[0].id or self.next_id > len(self.storage.items):
+                print("Задача с таким номером не найдена.")
+                continue
             for item in self.storage.items:
                 if item.id == self.next_id:
+                    if item.done is True:
+                        print('Задача была отмечена "выполненной" ранее!')
+                        return
                     item.done = True
                     self.storage.save()
+                    print("Задача выполнена!")
                     return
 
     def del_task(self):
@@ -67,8 +85,11 @@ class TaskManager:
                 print("Ошибка: введите число.")
                 continue
             self.storage.load()
-            if self.next_id < 1 or self.next_id > len(self.storage.items):
-                print("Задача с таким номером не найдена")
+            if not self.storage.items:
+                print("Список задач пуст.")
+                return
+            if self.next_id < self.storage.items[0].id or self.next_id > len(self.storage.items):
+                print("Задача с таким номером не найдена.")
                 continue
             for item in self.storage.items:
                 if item.id == self.next_id:
@@ -79,5 +100,3 @@ class TaskManager:
                     item.id -= 1
             self.storage.save()
             return
-
-    # TODO: доделать del_task
